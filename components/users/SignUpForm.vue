50086b110dd519ad49b8093a36338fc854f20e3b
Zhang Gang <gzhang@thoughtworks.com>
2019-08-19 01:21:48 +0800

    add loading

--- components/users/SignUpForm.vue
+++ components/users/SignUpForm.vue
@@ -1,29 +1,31 @@
 <template>
   <a-card class="login-form" title="Sign In">
-    <nuxt-link :to="{name:'users-sign-up'}" slot="extra">Sign Up</nuxt-link>
-    <a-form layout="horizontal" :form="form" @submit="handleSubmit">
-      <a-form-item>
-        <a-input v-decorator="decorator.username" size="large" placeholder="Username" ref="usernameInput">
-          <a-icon slot="prefix" type="user"/>
-        </a-input>
-      </a-form-item>
-      <a-form-item>
-        <a-input v-decorator="decorator.password" size="large" placeholder="Password" type="password"
-                 ref="passwordInput">
-          <a-icon slot="prefix" type="lock"/>
-        </a-input>
-      </a-form-item>
-      <a-form-item>
-        <a-checkbox>Remember me</a-checkbox>
-        <a class="forgot-password">Forgot password</a>
-      </a-form-item>
-      <a-form-item>
-        <a-button type="primary" size="large" html-type="submit" class="login-form-button">
-          Log in
-        </a-button>
-      </a-form-item>
-    </a-form>
-    <Sign-In-By-Others/>
+    <nuxt-link :to="{name:'users-sign-up'}" slot="extra">Or Sign Up</nuxt-link>
+    <a-spin :spinning="spinning">
+      <a-form layout="horizontal" :form="form" @submit="handleSubmit">
+        <a-form-item>
+          <a-input v-decorator="decorator.username" size="large" placeholder="Username" ref="usernameInput">
+            <a-icon slot="prefix" type="user"/>
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-input v-decorator="decorator.password" size="large" placeholder="Password" type="password"
+                   ref="passwordInput">
+            <a-icon slot="prefix" type="lock"/>
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-checkbox>Remember me</a-checkbox>
+          <a class="forgot-password">Forgot password</a>
+        </a-form-item>
+        <a-form-item>
+          <a-button type="primary" size="large" html-type="submit" class="login-form-button">
+            Log in
+          </a-button>
+        </a-form-item>
+      </a-form>
+      <Sign-In-By-Others/>
+    </a-spin>
   </a-card>
 </template>
 
@@ -37,6 +39,7 @@
         },
         data() {
             return {
+                spinning: false,
                 decorator: {
                     username: [
                         'username',
@@ -58,6 +61,7 @@
                 this.form.validateFields((err, values) => {
                     if (!err) {
                         console.log('Received values of form: ', values);
+                        this.spinning = true
                     }
                 });
             }
